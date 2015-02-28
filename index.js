be9c2efa27e8695a482c8152b8bdd2992677c66b
Arjun Gadhia <arjungadhia@gmail.com>
2015-02-28 21:51:13 +0000

    Use UK English

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ router.post('/checkLots', function*(next) {
 
 function getCorrection(str) {
     return function(done) {
-        gingerbread(str, { lang: 'GB'}, done);
+        gingerbread(str, { lang: 'UK'}, done);
     };
 }
 
