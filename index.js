39eabbd094f8349382d5521ce9bcbfb355f9bce2
Arjun Gadhia <arjungadhia@gmail.com>
2015-02-28 20:12:04 +0000

    Idiot. Rename the main app file

--- index.js
+++ index.js
@@ -7,14 +7,21 @@ var gingerbread = require('gingerbread');
 app.use(bodyParser());
 
 console.log('starting');
+console.log(process.env.PORT);
+console.log('2');
 
+
+console.log('router', router);
 router.get('/check/:string', function *(next){
+    console.log('getting corrections for', this.params.string);
     var correction = yield getCorrection(this.params.string);
     this.body = correction.slice(0,2);
     yield next;
 });
 
-router.post('/checkLots', function*(next) {
+router.post('/checkLots', function*(next) {i
+
+    console.log('recieved post req', this.request.body);
     if(!(this.request.body  && Array.isArray(this.request.body))) {
         this.body = { "error": "Input must be an array of strings" }
         this.status = 400;
@@ -39,4 +46,5 @@ function getCorrection(str) {
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-app.listen(3000);
+console.log('listening on port ' + (process.env.PORT || 3000));
+app.listen(process.env.PORT || 3000);
